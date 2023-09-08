a40c7155c57fe763005ea1dfbda16b3703b5dd26
kenliii <90875261+kenliii@users.noreply.github.com>
2023-09-07 21:36:29 -0700

    fixing bugs

--- src/components/Demo.jsx
+++ src/components/Demo.jsx
@@ -10,8 +10,21 @@ const Demo = () => {
     url: '',
     summary:'',
   });
+  const [allArticles, setAllArticles] = useState([]);
 
   const[getSummary, {error, isFetching}] = useLazyGetSummaryQuery();
+
+  useEffect(() => {
+    const articlesFromLocalStorage = JSON.parse(
+      localStorage.getItem('articles')
+    )
+
+    if(articlesFromLocalStorage) {
+      setAllArticles(articlesFromLocalStorage)
+    }
+
+
+  },[])
 
   const handleSubmit = async (e) => {
     e.preventDefault();
@@ -19,8 +32,12 @@ const Demo = () => {
 
     if(data?.summary) {
       const newArticle = { ...article, summary: data.summary};
+      const updatedAllArticles = [newArticle, ... allArticles];
 
       setArticle(newArticle);
+      setAllArticles(updatedAllArticles);
+
+      localStorage.setItem('articles', JSON.stringify(updatedAllArticles))
 
       console.log(newArticle);
     }
@@ -43,9 +60,7 @@ const Demo = () => {
             type="url"
             placeholder="Enter a URL"
             value={article.url}
-            onChange={(e)=> setArticle({
-              article, url: e.target.value
-            })}
+            onChange={(e)=> setArticle({ ...article, url: e.target.value })}
             required
             className="url_input peer"
           />
@@ -61,8 +76,55 @@ const Demo = () => {
         </form>
 
         {/* History */}
+        <div className="flex flex-col gap-1 max-h-60 overflow-y-auto">
+            {allArticles.map((item, index) =>(
+                <div
+                 key={`link-${index}`}
+                 onClick={() => setArticle(item)}
+                 className="link_card"
+                >
+                <div className="copy_btn">
+                  <img  
+                    src={copy}
+                    alt="copy_icon"
+                    className="w-[40%] h-[40%] object-contain"
+
+                  />
+                </div>
+                  <p className="flex-1 font-satoshi tex-blue-700 font-medium text-sm truncate">
+                    {item.url}
+                  </p>
+                </div>
+            ))}
+        </div>
       </div>
       {/* results */}
+      <div className='my-10 max-w-full flex justify-center items-center'>
+        {isFetching ? (
+          <img src={loader} alt='loader' className='w-20 h-20 object-contain' />
+        ) : error ? (
+          <p className='font-inter font-bold text-black text-center'>
+            Well, that wasn't supposed to happen...
+            <br />
+            <span className='font-satoshi font-normal text-gray-700'>
+              {error?.data?.error}
+            </span>
+          </p>
+        ) : (
+          article.summary && (
+            <div className='flex flex-col gap-3'>
+              <h2 className='font-satoshi font-bold text-gray-600 text-xl'>
+                Article <span className='blue_gradient'>Summary</span>
+              </h2>
+              <div className='summary_box'>
+                <p className='font-inter font-medium text-sm text-gray-700'>
+                  {article.summary}
+                </p>
+              </div>
+            </div>
+          )
+        )}
+      </div>
     </section>
   )
 }
--- src/components/Hero.jsx
+++ src/components/Hero.jsx
@@ -4,28 +4,29 @@ import { logo } from "../assets";
 
 const Hero = () => {
   return (
-    <header className="w-full flex justify-center items-center flex-col">
+    <section className="w-full flex justify-center items-center flex-col">
       <nav className="flex justify-between items-center w-full mb-10 pt-3">
-        <img  src={logo} alt="sumz_logo" className="w-28 object-contain" />
+        <img src={logo} alt="Sumz Logo" className="w-28 object-contain" />
 
         <button 
-            type="button" 
-            onClick={()=>window.open('https://github.com/kenliii')} 
-            className="black_btn">
-          Github
+          type="button" 
+          onClick={() => window.open('https://github.com/kenliii')} 
+          className="black_btn"
+        >
+          GitHub
         </button>
       </nav>
 
       <h1 className="head_text">
-        sumarize articles with <br className="max-md:hidden" /> 
+        Summarize articles with <br className="max-md:hidden" /> 
         <span className="orange_gradient">OpenAI GPT-4</span>
       </h1>
       <h2 className="desc">
         Make your reading easier with Summize, a free article summarization tool 
         that turns long articles into short and easy-to-understand summaries.
       </h2>
-    </header>
+    </section>
   )
 }
 
-export default Hero
+export default Hero;
